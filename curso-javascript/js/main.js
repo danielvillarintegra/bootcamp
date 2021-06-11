d868c85dd8c53016e4220dffe7ff9c8b1d9bae4a
danielvillarintegra <danielvillarintegra@gmail.com>
2021-06-11 16:55:39 -0300

    comit inicial

---
+++ curso-javascript/js/main.js
@@ -0,0 +1,17 @@
+/*
+*/
+var nome = "Rafael Gaelleani";
+var idade  = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+
+//alert("Bem vindo  " + nome + idade + idade2);
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toUpperCase);
+
+// console.log(frase.replace("Japão" , "Brasil"));
+frase = frase.replace("Japão" , "Brasil")
+alert(frase.toUpperCase());
+console.log(frase.toLowerCase);
