31d89058ceddb036ca3484b2962f7031c5c401de
danielvillarintegra <danielvillarintegra@gmail.com>
2021-06-12 13:28:57 -0300

    organizacao de arquivos

--- curso-javascript/js/main.js
+++ curso-javascript/js/main.js
@@ -1,5 +1,23 @@
+
+var d = new Date();
+alert(d.getMonth()+1);
 /*
+var idade = prompt("Qual sua idade:");
+
+if (idade>=18) {
+    alert("maior de idade");
+} else {
+    alert("menor de idade")
+}
 */
+/*
+var frutas =[{nome:"maça", cor:"vermelha"},{nome:"uva" , cor: "roxa"}]
+console.log(frutas[1].nome);
+alert(frutas[0].cor);
+*/
+
+/*
+
 var nome = "Rafael Gaelleani";
 var idade  = 29;
 var idade2 = 10;
@@ -14,4 +32,18 @@ console.log(frase.toUpperCase);
 // console.log(frase.replace("Japão" , "Brasil"));
 frase = frase.replace("Japão" , "Brasil")
 alert(frase.toUpperCase());
-console.log(frase.toLowerCase);
+console.log(frase.toLowerCase); */
+
+/*
+var lista = ["maça","pera", "laranja"];
+console.log(lista[2]);
+lista.push("uva");
+//lista.pop("pera");
+console.log(lista.length);
+//console.log(lista.reverse());
+console.log(lista[2]);
+console.log(lista.toString()[2]);
+console.log(lista);
+console.log(lista.join(" - "));
+*/
+
