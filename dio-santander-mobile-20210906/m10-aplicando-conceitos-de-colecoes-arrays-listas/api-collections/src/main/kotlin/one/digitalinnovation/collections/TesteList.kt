6b8312c626bc9eb204ac007202c125d36cf6a06f
danielvillarintegra <danielvillarintegra@gmail.com>
2021-07-14 20:24:39 -0300

    Testes com listOf e sortedBy

--- dio-santander-mobile-20210906/m10-aplicando-conceitos-de-colecoes-arrays-listas/api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ dio-santander-mobile-20210906/m10-aplicando-conceitos-de-colecoes-arrays-listas/api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,14 +1,20 @@
 package one.digitalinnovation.collections
 
 fun main (){
-    val joao = Funcionario("João", 1000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
+    val joao = Funcionario("João", 2000.0)
+    val pedro = Funcionario("Pedro", 1500.0)
     val maria = Funcionario("Maria", 4000.0)
 
     val funcionarios = listOf(joao, pedro, maria,)
 
+    println("a) Original -----------------")
     funcionarios.forEach { println(it)}
     println(funcionarios.find { it.nome == "Maria" })
+
+    println("b) Sort -----------------")
+    funcionarios
+        .sortedBy{it.salario}
+        .forEach { println(it)}
 
 }
 
