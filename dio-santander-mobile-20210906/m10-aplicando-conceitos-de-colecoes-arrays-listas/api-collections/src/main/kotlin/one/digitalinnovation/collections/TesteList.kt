a7100b88cab2f8dd8e4429a8786742108dcd3b6e
danielvillarintegra <danielvillarintegra@gmail.com>
2021-07-15 18:46:08 -0300

    Testes com setOf - union

--- dio-santander-mobile-20210906/m10-aplicando-conceitos-de-colecoes-arrays-listas/api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ dio-santander-mobile-20210906/m10-aplicando-conceitos-de-colecoes-arrays-listas/api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -2,40 +2,19 @@ package one.digitalinnovation.collections
 
 fun main (){
     val joao = Funcionario("João", 2000.0, "CLT")
-    val pedro = Funcionario("Pedro", 1500.0 , "CLT")
-    val maria = Funcionario("Maria", 4000.0 , "PJ")
+    val pedro = Funcionario("Pedro", 1500.0 , "PJ")
+    val maria = Funcionario("Maria", 4000.0 , "CLT")
 
-    val funcionarios = listOf(joao, pedro, maria,)
+    val funcionarios1 = setOf(joao, pedro)
+    val funcionarios2 = setOf(maria)
 
-    println("a) Original -----------------")
-    funcionarios.forEach { println(it)}
-    println(funcionarios.find { it.nome == "Maria" })
+    val resultUnion = funcionarios1.union(funcionarios2)
 
-    println("b) Sort -----------------")
-    funcionarios
-        .sortedBy{it.salario}
-        .forEach { println(it)}
+    println("a) União dos conjuntos funcionarios1 e funcionarios2 -----------------")
+    resultUnion.forEach { println(it)}
 
 
-    println("c) grupoby -----------------")
-    funcionarios
-        .groupBy{it.tipoContratacao}
-        .forEach { println(it)}
-
-}
-
-data class Funcionario(
-    val nome: String,
-    val salario: Double,
-    val tipoContratacao: String
-
-)  {
-    override fun toString(): String =
-        """
-            Nome: $nome
-            Salario: $salario
-
-        """.trimIndent()
 
 
 }
+
