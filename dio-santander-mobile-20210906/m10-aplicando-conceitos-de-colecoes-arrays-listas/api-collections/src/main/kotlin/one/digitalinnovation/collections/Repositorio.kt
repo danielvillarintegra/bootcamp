253f06924a1c291c56c601058100f05386382d81
danielvillarintegra <danielvillarintegra@gmail.com>
2021-07-16 20:11:13 -0300

    Testes com mutableMap

---
+++ dio-santander-mobile-20210906/m10-aplicando-conceitos-de-colecoes-arrays-listas/api-collections/src/main/kotlin/one/digitalinnovation/collections/Repositorio.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.collections
+
+class Repositorio<T> {
+    private val map = mutableMapOf<String, T>()
+    
+    fun create(id: String, value: T) {
+        map[id] = value
+
+    }
+
+    fun findById(id:String) = map[id]
+}
