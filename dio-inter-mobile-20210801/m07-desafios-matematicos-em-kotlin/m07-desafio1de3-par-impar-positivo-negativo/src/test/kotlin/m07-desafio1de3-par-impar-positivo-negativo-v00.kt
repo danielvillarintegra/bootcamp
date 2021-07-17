c1e69aa996b8e4f5cd404039a2761a2187bb05ca
danielvillarintegra <danielvillarintegra@gmail.com>
2021-07-17 13:04:28 -0300

    Desafio 1 de 3 commit inicial

---
+++ dio-inter-mobile-20210801/m07-desafios-matematicos-em-kotlin/m07-desafio1de3-par-impar-positivo-negativo/src/test/kotlin/m07-desafio1de3-par-impar-positivo-negativo-v00.kt
@@ -0,0 +1,33 @@
+import java.util.Scanner
+
+fun main() {
+
+    val inteiros = mutableListOf<Int>(-5, 0, -3, -4, 12)
+/* continue declarando as variaveis */
+    val pares = mutableListOf<Int>()
+    val impares = mutableListOf<Int>()
+    val positivos = mutableListOf<Int>()
+    val negativos = mutableListOf<Int>()
+
+    inteiros.forEach {println(it)}
+
+/* Seleciona os valores positivos e negativos */
+    inteiros.forEach {
+        if ( it > 0 ) {
+            positivos.add(it)
+        } else if ( it < 0 ) {
+            negativos.add(it)
+        }
+    }
+
+    inteiros.forEach {
+        if ((it.mod(2)) != 0) {
+            pares.add(it)
+        } else impares.add(it)
+    }
+
+    println("${pares.size} valor(es) par(es)")
+    println("${impares.size} valor(es) impar(es)")
+    println("${positivos.size} valor(es) positivo(s)")
+    println("${negativos.size} valor(es) negativo(s)")
+}
