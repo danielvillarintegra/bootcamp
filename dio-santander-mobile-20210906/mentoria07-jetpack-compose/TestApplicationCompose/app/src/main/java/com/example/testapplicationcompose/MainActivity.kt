b29be299c2759b4a8e6df2f3188b59f4b7987566
danielvillarintegra <danielvillarintegra@gmail.com>
2021-07-30 17:07:09 -0300

    Com Column

--- dio-santander-mobile-20210906/mentoria07-jetpack-compose/TestApplicationCompose/app/src/main/java/com/example/testapplicationcompose/MainActivity.kt
+++ dio-santander-mobile-20210906/mentoria07-jetpack-compose/TestApplicationCompose/app/src/main/java/com/example/testapplicationcompose/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.testapplicationcompose
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Cyan
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.testapplicationcompose.ui.theme.TestApplicationComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,14 +36,25 @@ class MainActivity : ComponentActivity() {
 fun MainsScreen(name: String) {
     Column {
         Text(text = "Olá $name!")
-        Text(text = "Texto embaixo!", Modifier.padding(8))
+        Text(
+            text = "Texto embaixo!",
+            Modifier
+                .padding(8.dp)
+                .background(color = Cyan)
+                .padding(12.dp)
 
+        )
+        Button(onClick = { }) {
+            Text(text = "Texto do botão")
+        }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TestApplicationComposeTheme {
-        Greeting("Android")
+        MainsScreen("Android")
     }
+}
 }
