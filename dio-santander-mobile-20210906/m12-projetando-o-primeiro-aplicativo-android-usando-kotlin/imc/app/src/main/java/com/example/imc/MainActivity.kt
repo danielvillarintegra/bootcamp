3b57a0a0dff269ecaae2c798384aede2448088e0
danielvillarintegra <danielvillarintegra@gmail.com>
2021-08-02 20:18:45 -0300

    Executando, mas com falha na transferência dos valores entre os componentes

--- dio-santander-mobile-20210906/m12-projetando-o-primeiro-aplicativo-android-usando-kotlin/imc/app/src/main/java/com/example/imc/MainActivity.kt
+++ dio-santander-mobile-20210906/m12-projetando-o-primeiro-aplicativo-android-usando-kotlin/imc/app/src/main/java/com/example/imc/MainActivity.kt
@@ -1,36 +1,70 @@
 package com.example.imc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Half.toFloat
-import android.widget.EditText
-import android.widget.TextView
-import kotlin.Unit.toString
-
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setListeners()
     }
-}
 
-fun setListeners() {
-        val editTextAltura : EditText = findViewById(R.id.edt_altura)
-        val editTextPeso : EditText = findViewById<EditText>(R.id.edt_peso)
-        val textViewResultado : TextView
-        val altura = editTextAltura.getText()
-        val peso = editTextPeso.getText()
+    private fun setListeners() {
+        val alturaString : String = edt_altura.text.toString()
+        val pesoString : String = edt_peso.text.toString()
+        var pesoFloat : Float = 0.0F
+        var alturaFloat : Float = 0.0F
+        var calcularIMC : Float = 0.0F
 
-        val calcularIMC = peso.times(altura.times(altura))
-        textViewResultado.text = toString(calcularIMC)
+        /*
+        edt_altura.doAfterTextChanged {
+            if (alturaString.isNullOrEmpty() == false) {
+                Toast.makeText(
+                    this,
+                    "please input data, edit text cannot be blank",
+                    Toast.LENGTH_LONG
+                ).show()
+                tvw_resultado1.setText("= " + alturaString).toString()
+            } else {
+                alturaFloat = alturaString.toFloat()
+                tvw_resultado1.setText("Altura = " + alturaString).toString()
+            }
+        }
+
+        edt_peso.doAfterTextChanged {
+            if (pesoString.isNullOrEmpty() == false) {
+                Toast.makeText(
+                    this,
+                    "please input data, edit text cannot be blank",
+                    Toast.LENGTH_LONG
+                ).show()
+                tvw_resultado2.setText("= " + pesoString).toString()
+            } else {
+                pesoFloat = pesoString.toFloat()
+                tvw_resultado2.setText("Peso = " + pesoString).toString()
+            }
+        }
+        */
+
+
+        btn_calcular.setOnClickListener() {
+            alturaFloat = 1.79F
+            pesoFloat = 84.0F
+            calcularIMC = pesoFloat.div(alturaFloat.times(alturaFloat))
+            tvw_resultado1.setText("$alturaFloat").toString()
+            tvw_resultado2.setText("$pesoFloat").toString()
+            tvw_resultado.setText("$calcularIMC").toString()
+
+        }
 
     }
-
-/*
-private fun calcularIMC(altura: Float, peso: Float) : CharSequence {
-    return calcularIMC() = peso.times (altura.times(altura))
-
 }
-*/
+
+
