8452500c82bbc9d30cdf476c599cef945ae778a4
danielvillarintegra <danielvillarintegra@gmail.com>
2021-08-03 11:49:29 -0300

    Funcionando ao clicar no botão calcular. When da tabela imc criado, mas rever a precisão do float pois, está gerando valores fora do range (in x.0 .. y.0) e segue para o else (no when usei precisão de uma casa decimal). Próximo passo: Implementar o arredondamento do float para uma casa decimal e encaixar perfeitamente no when da tabela imc.

--- dio-santander-mobile-20210906/m12-projetando-o-primeiro-aplicativo-android-usando-kotlin/imc/app/src/main/java/com/example/imc/MainActivity.kt
+++ dio-santander-mobile-20210906/m12-projetando-o-primeiro-aplicativo-android-usando-kotlin/imc/app/src/main/java/com/example/imc/MainActivity.kt
@@ -3,12 +3,9 @@ package com.example.imc
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Half.toFloat
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.doAfterTextChanged
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -81,6 +78,7 @@ class MainActivity : AppCompatActivity() {
         btn_calcular.setOnClickListener() {
             val alturaString : String = edt_altura.text.toString()
             val pesoString : String = edt_peso.text.toString()
+            var resultadoTabela: String = ""
             var pesoFloat : Float
             var alturaFloat : Float
             var calcularIMC : Float = 0.0F
@@ -91,10 +89,55 @@ class MainActivity : AppCompatActivity() {
             alturaFloat = alturaString.toFloat()
             pesoFloat = pesoString.toFloat()
             calcularIMC = pesoFloat.div(alturaFloat.times(alturaFloat))
-            tvw_resultado1.setText("$alturaFloat").toString()
+
+            /*Incluir arredondamento 1 casa decimal */
+
+
             tvw_resultado2.setText("$pesoFloat").toString()
+            tvw_resultado_anterior.setText("$calcularIMC").toString()
             tvw_resultado.setText("$calcularIMC").toString()
 
+
+            when (calcularIMC) {
+                in 16.0 .. 16.9  -> {
+                    resultadoTabela = "Muito abaixo do peso: $calcularIMC"
+                    tvw_resultado.setText(resultadoTabela).toString()
+                }
+
+                in 17.0 .. 18.4  -> {
+                    tvw_resultado.setText("Abaixo do peso: $calcularIMC").toString()
+                }
+
+                in 18.5 .. 24.9  -> {
+                    resultadoTabela = "Normal: $calcularIMC"
+                    tvw_resultado.setText(resultadoTabela).toString()
+                }
+
+                in 25.0 .. 29.9  -> {
+                    resultadoTabela = "Acima do peso: $calcularIMC"
+                    tvw_resultado.setText(resultadoTabela).toString()
+                }
+
+                in 30.0 .. 34.9  -> {
+                resultadoTabela = "Obesidade grau 1: $calcularIMC"
+                tvw_resultado.setText(resultadoTabela).toString()
+                }
+
+                in 35.0 .. 40.0  -> {
+                resultadoTabela = "Obesidade grau 2: $calcularIMC"
+                tvw_resultado.setText(resultadoTabela).toString()
+                }
+
+                in 40.0 .. 50.0 -> {
+                resultadoTabela = "Obesidade grau 3: $calcularIMC"
+                tvw_resultado.setText(resultadoTabela).toString()
+                }
+            else -> {
+                resultadoTabela = "Fora da tabela: $calcularIMC"
+                }
+            }
+            tvw_resultado_anterior.setText("$calcularIMC").toString()
+            tvw_resultado.setText(resultadoTabela).toString()
 
         }
 
