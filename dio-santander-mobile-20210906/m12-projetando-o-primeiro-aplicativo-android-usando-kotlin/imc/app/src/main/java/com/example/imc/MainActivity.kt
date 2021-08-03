6afe3f4b38c6484ea7099230e49d3018c7af000f
danielvillarintegra <danielvillarintegra@gmail.com>
2021-08-03 10:20:28 -0300

    Funcionando ao clicar no botão calcular. Proximo passo: Implementar a tabela na qual o imc se enquadra;

--- dio-santander-mobile-20210906/m12-projetando-o-primeiro-aplicativo-android-usando-kotlin/imc/app/src/main/java/com/example/imc/MainActivity.kt
+++ dio-santander-mobile-20210906/m12-projetando-o-primeiro-aplicativo-android-usando-kotlin/imc/app/src/main/java/com/example/imc/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.imc
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Half.toFloat
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -17,50 +19,82 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setListeners() {
-        val alturaString : String = edt_altura.text.toString()
-        val pesoString : String = edt_peso.text.toString()
-        var pesoFloat : Float = 0.0F
-        var alturaFloat : Float = 0.0F
-        var calcularIMC : Float = 0.0F
 
-        /*
-        edt_altura.doAfterTextChanged {
-            if (alturaString.isNullOrEmpty() == false) {
+        edt_altura.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
                 Toast.makeText(
-                    this,
-                    "please input data, edit text cannot be blank",
-                    Toast.LENGTH_LONG
+                    applicationContext,
+                    "executed before making any change over EditText + $p0",
+                    Toast.LENGTH_SHORT
                 ).show()
-                tvw_resultado1.setText("= " + alturaString).toString()
-            } else {
-                alturaFloat = alturaString.toFloat()
-                tvw_resultado1.setText("Altura = " + alturaString).toString()
             }
-        }
 
-        edt_peso.doAfterTextChanged {
-            if (pesoString.isNullOrEmpty() == false) {
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
                 Toast.makeText(
-                    this,
-                    "please input data, edit text cannot be blank",
-                    Toast.LENGTH_LONG
+                    applicationContext,
+                    "executed while making any change over EditText  + $p0",
+                    Toast.LENGTH_SHORT
                 ).show()
-                tvw_resultado2.setText("= " + pesoString).toString()
-            } else {
-                pesoFloat = pesoString.toFloat()
-                tvw_resultado2.setText("Peso = " + pesoString).toString()
             }
-        }
-        */
 
+            override fun afterTextChanged(p0: Editable?) {
+                //  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                Toast.makeText(
+                    applicationContext,
+                    "executed after change made over EditText + $p0",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+
+        edt_peso.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                Toast.makeText(
+                    applicationContext,
+                    "Peso: executado antes de fazer alguma alteração sobre EditText",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                Toast.makeText(
+                    applicationContext,
+                    "Peso: executado enquanto faz qualquer alteração sobre o EditText",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                //  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                Toast.makeText(
+                    applicationContext,
+                    "Peso: executado após as alterações feitas sobre o EditText",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
 
         btn_calcular.setOnClickListener() {
-            alturaFloat = 1.79F
+            val alturaString : String = edt_altura.text.toString()
+            val pesoString : String = edt_peso.text.toString()
+            var pesoFloat : Float
+            var alturaFloat : Float
+            var calcularIMC : Float = 0.0F
+
+         /*   alturaFloat = 1.79F
             pesoFloat = 84.0F
+           */
+            alturaFloat = alturaString.toFloat()
+            pesoFloat = pesoString.toFloat()
             calcularIMC = pesoFloat.div(alturaFloat.times(alturaFloat))
             tvw_resultado1.setText("$alturaFloat").toString()
             tvw_resultado2.setText("$pesoFloat").toString()
             tvw_resultado.setText("$calcularIMC").toString()
+
 
         }
 
