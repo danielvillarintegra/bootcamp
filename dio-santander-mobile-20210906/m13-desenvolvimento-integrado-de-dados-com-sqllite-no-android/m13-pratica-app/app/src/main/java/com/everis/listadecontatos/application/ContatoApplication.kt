9daa3b6ca5d906071733328d9ab2e18d0877af62
danielvillarintegra <danielvillarintegra@gmail.com>
2021-08-05 10:54:40 -0300

    var helperDB (private set) e onCreate

--- dio-santander-mobile-20210906/m13-desenvolvimento-integrado-de-dados-com-sqllite-no-android/m13-pratica-app/app/src/main/java/com/everis/listadecontatos/application/ContatoApplication.kt
+++ dio-santander-mobile-20210906/m13-desenvolvimento-integrado-de-dados-com-sqllite-no-android/m13-pratica-app/app/src/main/java/com/everis/listadecontatos/application/ContatoApplication.kt
@@ -1,11 +1,16 @@
 package com.everis.listadecontatos.application
 
 import android.app.Application
+import com.everis.listadecontatos.helpers.HelperDB
 
 
 class ContatoApplication : Application(){
+    var helperDB : HelperDB? = null
+        private set //* Bloqueia a variável para "set" externo. Só aceitará "get"
+
     override fun onCreate() {
         super.onCreate()
+        helperDB = HelperDB(this)
     }
 
 }
