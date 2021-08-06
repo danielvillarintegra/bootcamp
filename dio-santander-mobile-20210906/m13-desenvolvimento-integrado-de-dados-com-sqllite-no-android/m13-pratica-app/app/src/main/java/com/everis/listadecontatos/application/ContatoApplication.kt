603a256d16ba556c43be7909e2d08d578948b431
danielvillarintegra <danielvillarintegra@gmail.com>
2021-08-05 21:13:55 -0300

    m13-App com bug: a) não salva contato b) trava após duas inserções c) não busca :(

--- dio-santander-mobile-20210906/m13-desenvolvimento-integrado-de-dados-com-sqllite-no-android/m13-pratica-app/app/src/main/java/com/everis/listadecontatos/application/ContatoApplication.kt
+++ dio-santander-mobile-20210906/m13-desenvolvimento-integrado-de-dados-com-sqllite-no-android/m13-pratica-app/app/src/main/java/com/everis/listadecontatos/application/ContatoApplication.kt
@@ -3,13 +3,18 @@ package com.everis.listadecontatos.application
 import android.app.Application
 import com.everis.listadecontatos.helpers.HelperDB
 
-
 class ContatoApplication : Application(){
+
     var helperDB : HelperDB? = null
         private set //* Bloqueia a variável para "set" externo. Só aceitará "get"
 
+    companion object {
+        lateinit var instance: ContatoApplication
+    }
+
     override fun onCreate() {
         super.onCreate()
+        instance = this
         helperDB = HelperDB(this)
     }
 
--- dio-santander-mobile-20210906/m13-desenvolvimento-integrado-de-dados-com-sqllite-no-android/m13-pratica-app/app/src/main/java/com/everis/listadecontatos/helpers/HelperDB.kt
+++ dio-santander-mobile-20210906/m13-desenvolvimento-integrado-de-dados-com-sqllite-no-android/m13-pratica-app/app/src/main/java/com/everis/listadecontatos/helpers/HelperDB.kt
@@ -1,8 +1,10 @@
 package com.everis.listadecontatos.helpers
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.everis.listadecontatos.feature.listacontatos.model.ContatosVO
 
 // tem que herdar a classe SQLiteOpenHelper
 class HelperDB(
@@ -21,9 +23,10 @@ class HelperDB(
 
     val DROP_TABLE = "DROP TABLE IF EXISTS $TABLE_NAME"
     val CREATE_TABLE = "CREATE TABLE $TABLE_NAME (" +
-                            "$COLUMNS_ID INTEGER NOT NUL," +
-                            "$COLUMNS_NOME TEXT NOT NUL," +
-                            "$COLUMNS_TELEFONE TEXT NOT NUL," +
+                            "$COLUMNS_ID INTEGER NOT NULL," +
+                            "$COLUMNS_NOME TEXT NOT NULL," +
+                            "$COLUMNS_TELEFONE TEXT NOT NULL," +
+                        "" +
                         "PRIMARY KEY($COLUMNS_ID AUTOINCREMENT" +
                         ")"
 
@@ -38,4 +41,60 @@ class HelperDB(
         }
         onCreate(db)
     }
+
+    fun buscarContatos(busca: String) : List<ContatosVO> {
+        salvarContato(ContatosVO(0, "TESTE 2", "TESTE 2"))
+
+        val db = readableDatabase ?: return mutableListOf() // Elvis: se o DataBase for nulo retorna um tipo mutableListOf() vazio
+        var lista = mutableListOf<ContatosVO>()
+        val sql = "SELECT * FROM $TABLE_NAME WHERE $COLUMNS_NOME LIKE '%$busca'%"
+        val buscaComPercentual = "%$busca%"
+        var cursor = db.rawQuery(sql, arrayOf(buscaComPercentual))  // ?: return mutableListOf() se usar Elvis: se o DataBase for nulo não faz nada, mas retorna um tipo mutableListOf() vazio
+        if (cursor == null){
+            db.close()
+            return mutableListOf()
+        }
+
+        while (cursor.moveToNext()) {
+            var contato = ContatosVO(
+                cursor.getInt(cursor.getColumnIndex(COLUMNS_ID)),	/* De Para */
+                cursor.getString(cursor.getColumnIndex(COLUMNS_NOME)),	/* De Para */
+                cursor.getString(cursor.getColumnIndex(COLUMNS_TELEFONE))	/* De Para */
+            )
+            lista.add(contato)
+        }
+        db.close()
+        return lista
+    }
+/* Terceira maneira de fazer INSERT */
+    fun salvarContato(contato: ContatosVO) {
+        var db = writableDatabase ?: return
+        var content = ContentValues()
+        content.put(COLUMNS_NOME,contato.nome)
+        content.put(COLUMNS_TELEFONE,contato.telefone)
+        db.insert(TABLE_NAME, null, content)
+        db.close()
+    }
+
+/* Primeira maneira de fazer INSERT
+    fun salvarContato(contato: ContatosVO) {
+        var db = writableDatabase ?: return
+        val sql = "INSERT INTO $TABLE_NAME ($COLUMNS_NOME,$COLUMNS_TELEFONE) VALUES ('TESTE','TESTE')"
+        db.execSQL(sql)
+        db.close()
+    }
+
+ */
+
+ /* Segunda maneira de fazer INSERT
+    fun salvarContato(contato: ContatosVO) {
+        var db = writableDatabase ?: return
+
+        val sql = "INSERT INTO $TABLE_NAME ($COLUMNS_NOME,$COLUMNS_TELEFONE) VALUES (?,?)"
+        var array = arrayOf(contato.nome, contato.telefone)
+        db.execSQL(sql, array)
+        db.close()
+}
+*/
+
 }
