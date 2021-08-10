0ab5976cf1763c0f143d21c79c594a860b2f261f
danielvillarintegra <danielvillarintegra@gmail.com>
2021-08-10 09:59:36 -0300

    m15-preparação do módulo

---
+++ dio-santander-mobile-20210906/m15-conceitos-de-navegacao-aparencia-e-estilo-de-aplicativos-android/m15-pratica-app/app/src/main/java/com/example/appcontatos/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.appcontatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private val rvList: RecyclerView by lazy {  //
+        findViewById<RecyclerView>(R.id.rv_List)
+
+    }
+
+    private val adapter = ContactAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bindViews()
+    }
+
+    private fun bindViews(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList(){
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    "Igor Ferrani"
+                    "(00) 0000-0000"
+                    "img.png"
+                )
+            )
+        )
+    }
+
+}
