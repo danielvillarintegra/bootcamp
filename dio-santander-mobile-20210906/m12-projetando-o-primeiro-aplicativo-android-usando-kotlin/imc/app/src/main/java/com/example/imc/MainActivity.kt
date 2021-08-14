1917e8ffbdcdf63c0a4c3b29a0424b9e7d40ba3f
danielvillarintegra <danielvillarintegra@gmail.com>
2021-08-13 23:18:40 -0300

    m12-Adequação da forma de programação, baseada no módulo 16 (m16-VModel).

--- dio-santander-mobile-20210906/m12-projetando-o-primeiro-aplicativo-android-usando-kotlin/imc/app/src/main/java/com/example/imc/MainActivity.kt
+++ dio-santander-mobile-20210906/m12-projetando-o-primeiro-aplicativo-android-usando-kotlin/imc/app/src/main/java/com/example/imc/MainActivity.kt
@@ -1,23 +1,86 @@
 package com.example.imc
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.Toast
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btCalcular:Button
+    lateinit var etAltura:EditText
+    lateinit var etPeso:EditText
+    lateinit var tvResultado:TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setListeners()
+        initDados()
+        initCalcular()
+
     }
 
-    private fun setListeners() {
+    private fun initDados(){
+        etAltura = findViewById(R.id.etAltura)
+        etPeso = findViewById(R.id.etPeso)
+        btCalcular = findViewById(R.id.btCalcular)
+        tvResultado = findViewById(R.id.tvResultado)
+    }
 
-        edt_altura.addTextChangedListener(object : TextWatcher {
+    private fun initCalcular() {
+        val altura  = etAltura.text.toString().toFloat()
+        val peso = etPeso.text.toString().toFloat()
+        val calcularIMC = (peso.div(altura.times(altura)))
+
+        /*Incluir arredondamento 1 casa decimal */
+        /* Talvez converter o float para String e depois arredondar ?
+
+         */
+        var resultadoTabela = String.format("%.1f", calcularIMC)
+
+        tvResultado.setText("$calcularIMC").toString()
+
+        when ((resultadoTabela).toDouble()) {
+            in 16.0 .. 16.9  -> {
+                resultadoTabela = "Muito abaixo do peso: $calcularIMC"
+            }
+
+            in 17.0 .. 18.4  -> {
+                resultadoTabela ="Abaixo do peso: $calcularIMC"
+            }
+
+            in 18.5 .. 24.9  -> {
+                resultadoTabela = "Normal: $calcularIMC"
+            }
+
+            in 25.0 .. 29.9  -> {
+                resultadoTabela = "Acima do peso: $calcularIMC"
+            }
+
+            in 30.0 .. 34.9  -> {
+                resultadoTabela = "Obesidade grau 1: $calcularIMC"
+            }
+
+            in 35.0 .. 40.0  -> {
+                resultadoTabela = "Obesidade grau 2: $calcularIMC"
+            }
+
+            else -> {
+                if (((resultadoTabela).toDouble()) > 40.0) resultadoTabela = "Obesidade grau 3: $calcularIMC"
+                else resultadoTabela = "Fora da tabela: $calcularIMC"
+            }
+        }
+
+        tvResultado.setText(resultadoTabela).toString()
+
+    }
+
+    /*private fun setListeners() {
+
+        etAltura.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 //  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
                 Toast.makeText(
@@ -46,7 +109,7 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        edt_peso.addTextChangedListener(object : TextWatcher {
+        etPeso.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 //  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
                 Toast.makeText(
@@ -75,73 +138,66 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        btn_calcular.setOnClickListener() {
-            val alturaString : String = edt_altura.text.toString()
-            val pesoString : String = edt_peso.text.toString()
-            var resultadoTabela: String = ""
-            var pesoFloat : Float
-            var alturaFloat : Float
-            var calcularIMC : Float = 0.0F
+        btCalcular.setOnClickListener() {
+            val alturaString : String = findViewById(edAltura).text.toString()
+            val pesoString : String = etPeso.text.toString()
+            val pesoFloat : Float
+            val alturaFloat : Float
+            val calcularIMC : Float
 
-         /*   alturaFloat = 1.79F
+
+         *//*   alturaFloat = 1.79F
             pesoFloat = 84.0F
-           */
+           *//*
             alturaFloat = alturaString.toFloat()
             pesoFloat = pesoString.toFloat()
-            calcularIMC = pesoFloat.div(alturaFloat.times(alturaFloat))
+            calcularIMC = (pesoFloat.div(alturaFloat.times(alturaFloat)))
 
-            /*Incluir arredondamento 1 casa decimal */
+            *//*Incluir arredondamento 1 casa decimal *//*
+            *//* Talvez converter o float para String e depois arredondar ?
+
+             *//*
+            var resultadoTabela = String.format("%.1f", calcularIMC)
+
+            tvResultado.setText("$calcularIMC").toString()
 
 
-            tvw_resultado2.setText("$pesoFloat").toString()
-            tvw_resultado_anterior.setText("$calcularIMC").toString()
-            tvw_resultado.setText("$calcularIMC").toString()
-
-
-            when (calcularIMC) {
+            when ((resultadoTabela).toDouble()) {
                 in 16.0 .. 16.9  -> {
                     resultadoTabela = "Muito abaixo do peso: $calcularIMC"
-                    tvw_resultado.setText(resultadoTabela).toString()
                 }
 
                 in 17.0 .. 18.4  -> {
-                    tvw_resultado.setText("Abaixo do peso: $calcularIMC").toString()
+                    resultadoTabela ="Abaixo do peso: $calcularIMC"
                 }
 
                 in 18.5 .. 24.9  -> {
                     resultadoTabela = "Normal: $calcularIMC"
-                    tvw_resultado.setText(resultadoTabela).toString()
                 }
 
                 in 25.0 .. 29.9  -> {
                     resultadoTabela = "Acima do peso: $calcularIMC"
-                    tvw_resultado.setText(resultadoTabela).toString()
                 }
 
                 in 30.0 .. 34.9  -> {
                 resultadoTabela = "Obesidade grau 1: $calcularIMC"
-                tvw_resultado.setText(resultadoTabela).toString()
                 }
 
                 in 35.0 .. 40.0  -> {
                 resultadoTabela = "Obesidade grau 2: $calcularIMC"
-                tvw_resultado.setText(resultadoTabela).toString()
                 }
 
-                in 40.0 .. 50.0 -> {
-                resultadoTabela = "Obesidade grau 3: $calcularIMC"
-                tvw_resultado.setText(resultadoTabela).toString()
-                }
             else -> {
-                resultadoTabela = "Fora da tabela: $calcularIMC"
+                    if (((resultadoTabela).toDouble()) > 40.0) resultadoTabela = "Obesidade grau 3: $calcularIMC"
+                    else resultadoTabela = "Fora da tabela: $calcularIMC"
                 }
             }
-            tvw_resultado_anterior.setText("$calcularIMC").toString()
-            tvw_resultado.setText(resultadoTabela).toString()
+
+            tvResultado.setText(resultadoTabela).toString()
 
         }
 
-    }
+    }*/
 }
 
 
