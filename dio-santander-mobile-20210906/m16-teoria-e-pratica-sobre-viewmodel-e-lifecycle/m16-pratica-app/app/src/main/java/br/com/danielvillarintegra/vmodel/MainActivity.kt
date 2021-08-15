6aeea89bd53ffb80409eb4ac33ce469f7a53da76
danielvillarintegra <danielvillarintegra@gmail.com>
2021-08-15 11:42:07 -0300

    m16-Regras de negócios separada da UI e funcionando; Botão calcular funcionando; Implementado Observer e MutableLiveData<String>;
    Falta implementar o botão mostrar e toast.

--- dio-santander-mobile-20210906/m16-teoria-e-pratica-sobre-viewmodel-e-lifecycle/m16-pratica-app/app/src/main/java/br/com/danielvillarintegra/vmodel/MainActivity.kt
+++ dio-santander-mobile-20210906/m16-teoria-e-pratica-sobre-viewmodel-e-lifecycle/m16-pratica-app/app/src/main/java/br/com/danielvillarintegra/vmodel/MainActivity.kt
@@ -6,87 +6,44 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var txtContador: EditText
     lateinit var btnDados: Button
     lateinit var btnMostrar:Button
-    var contador : Int = 0
+    lateinit var mViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initDados()
-        initContador()
         initClick()
-        validaContador()
-        logar(valor = "ONCREATE")
-
     }
 
-    override fun onStart() {
-        super.onStart()
-        logar(valor = "ONSTART")
+    private fun initDados() {
+        mViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-    }
-
-    override fun onResume() {
-        super.onResume()
-        logar(valor = "ONRESUME")
-
-    }
-
-    override fun onPause() {
-        super.onPause()
-        logar(valor = "PAUSE")
-
-    }
-
-    override fun onStop() {
-        super.onStop()
-        logar(valor = "ONSTOP")
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        logar(valor = "ONDESTROY")
-
-    }
-
-    private fun logar(tag:String = "Ciclo de vida", valor:String) {
-        Log.d(tag,valor)
-    }
-
-    private fun validaContador() {
-        if (contador > 5) {
-            contador = 0
-        }
+        txtContador = findViewById(R.id.etContador)
+        btnDados = findViewById(R.id.btnDados)
+        btnMostrar = findViewById(R.id.btnMostrar)
+        mViewModel.mContador.observe(this, Observer { valor ->
+            txtContador.setText(valor)
+        })
     }
 
     private fun initClick() {
         btnDados.setOnClickListener {
-            contador++
-            validaContador()
-            initContador()
+            mViewModel.Contador()
         }
-
         btnMostrar.setOnClickListener{
+/*
             Toast.makeText(this,
                 "Valor contador: ${contador.toString()}", Toast.LENGTH_SHORT).show()
+*/
         }
     }
-
-    private fun initContador(){
-        txtContador.setText(contador.toString())
-    }
-
-    private fun initDados(){
-        txtContador = findViewById(R.id.etContador)
-        btnDados = findViewById(R.id.btnDados)
-        btnMostrar = findViewById(R.id.btnMostrar)
-    }
-
-
 }
