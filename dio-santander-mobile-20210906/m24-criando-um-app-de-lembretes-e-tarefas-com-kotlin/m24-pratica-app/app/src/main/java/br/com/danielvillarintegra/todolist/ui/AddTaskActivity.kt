7a2377e97eee834f48f01de485735db9fe22e980
danielvillarintegra <danielvillarintegra@gmail.com>
2021-08-28 21:57:47 -0300

    m24-App To Do List: UI construídas

---
+++ dio-santander-mobile-20210906/m24-criando-um-app-de-lembretes-e-tarefas-com-kotlin/m24-pratica-app/app/src/main/java/br/com/danielvillarintegra/todolist/ui/AddTaskActivity.kt
@@ -0,0 +1,12 @@
+package br.com.danielvillarintegra.todolist.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.danielvillarintegra.todolist.databinding.ActivityAddTaskBinding
+
+class AddTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ActivityAddTaskBinding
+    }
+}
