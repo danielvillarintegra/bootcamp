d28b830f06620fb8ddf3ea20a07899db1b50b089
Daniel Villar <danielvillarintegra@gmail.com>
2021-10-13 19:00:15 -0300

    Acrescentado evento onClick ReadMore para abertura e leitura do README.md nos repositórios encontrados.

---
+++ dio-inter-mobile-20210801/m25-criando-um-app-para-apresentar-seu-portfolio-do-github/app-repositories-master/app/src/main/java/br/com/dio/app/repositories/ui/MainActivity.kt
@@ -0,0 +1,88 @@
+package br.com.dio.app.repositories.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
+import br.com.dio.app.repositories.R
+import br.com.dio.app.repositories.core.createDialog
+import br.com.dio.app.repositories.core.createProgressDialog
+import br.com.dio.app.repositories.core.hideSoftKeyboard
+import br.com.dio.app.repositories.databinding.ActivityMainBinding
+import br.com.dio.app.repositories.presentation.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    private val dialog by lazy { createProgressDialog() }
+    private val viewModel by viewModel<MainViewModel>()
+    private val adapter by lazy { RepoListAdapter() }
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        binding.rvRepos.adapter = adapter
+
+        viewModel.repos.observe(this) {
+            when (it) {
+                MainViewModel.State.Loading -> dialog.show()
+                is MainViewModel.State.Error -> {
+                    createDialog {
+                        setMessage(it.error.message)
+                    }.show()
+                    dialog.dismiss()
+                }
+                is MainViewModel.State.Success -> {
+                    dialog.dismiss()
+                    adapter.submitList(it.list)
+                }
+            }
+        }
+        insertListeners()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        query?.let { viewModel.getRepoList(it) }
+        binding.root.hideSoftKeyboard()
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.d(TAG, "onQueryTextChange: $newText")
+        return false
+    }
+
+    companion object {
+        private const val TAG = "TAG"
+    }
+
+    private fun insertListeners() {
+        binding.rvRepos.setOnClickListener {
+            Log.d("D/Debug", "Click em rvRepos")
+        }
+
+        adapter.listenerOpen = { repo ->
+            val intent = Intent(this, ReadMeActivity::class.java)
+            val urlReadMe : String = repo.htmlURL + "/blob/main/README.md" //.getText "https://github.com/danielvillarintegra/bootcamp/blob/main/README.md"
+            intent.putExtra("url", urlReadMe)
+            startActivity(intent)
+            Log.d("D/Sucesso","Click em open readme $repo")
+        }
+
+    }
+
+
+}
